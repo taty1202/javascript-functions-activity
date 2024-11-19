23c7f12d2a91088d20c6baaecdec3f975e6e4c75
Adrian Prado <adrian.prado@adadevelopersacademy.org>
2024-11-19 10:36:21 -0700

    Added index.js file

---
+++ src/index.js
@@ -0,0 +1,35 @@
+function divides(num1, num2) {
+    return num1 % num2 === 0
+  }
+  
+  // Convert the above function to arrow function
+  
+  
+  
+  // Write a function that converts Fahrenheit to Celsius 
+  
+  
+  
+  
+  // Convert the python function below to a javascript function
+  
+  // def pairs_with_given_sum(numbers, target):
+  //     total = 0
+  //     for i in range(len(numbers)-1):
+  //         for j in range(i+1, len(numbers)):
+  //             if numbers[i]+numbers[j] == target:
+  //                 total += 1
+  //     return total
+  
+  
+  // Using the setInterval function (described below), register a callback function to be called every second.
+  // Consider printing some output so that you can see that the callback is running.
+  // setInterval is very similar to the setTimeout function described in Learn, taking the form
+  //
+  // setInterval(callBackFunction, intervalInMs)
+  //   callBackFunction is a reference to the function to be run periodically
+  //   intervalInMs is how much time should pass between calls to callBackFunction (in millisecond units)
+  //
+  // Check out the MDN documentation for setInterval (and setTimeout) for additional details
+  
+  // add your call here
